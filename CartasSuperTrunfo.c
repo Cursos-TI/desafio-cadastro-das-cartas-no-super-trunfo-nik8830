c62d8f4341610f0dd4d0d98a9342c8206552dd8d
nik8830 <168482527+nik8830@users.noreply.github.com>
2025-01-14 17:55:18 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,51 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main(){
+//Data for each card.
+    struct CartasSuperTrunfo
+    {
+        char cardCode[3];
+        int population;
+        int area;
+        int PIB;
+        int touristAttractions;
+        float populationDensity;
+        float pibPerCapta;
+    };
+    struct CartasSuperTrunfo Cards[2];
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+//Data input to register each card.
+    int i;
+    for (i=0; i<2; i++){
+        int numeroCarta = i+1;
+        printf("\nDigite o codigo da carta %d: (Exemplo:A03)\n", numeroCarta);
+        scanf("%s", &Cards[i].cardCode);
 
-    return 0;
+        printf("\nDigite a populacao:\n");
+        scanf("%d", &Cards[i].population);
+
+        printf("\nDigite a área da cidade em Km quadrados:\n");
+        scanf("%d", &Cards[i].area);
+
+        printf("\nDigite o PIB da cidade:\n");
+        scanf("%d", &Cards[i].PIB);
+
+        printf("\nDigite o numero de atracoes turisticas da Cidade:\n");
+        scanf("%d", &Cards[i].touristAttractions);
+        
+        Cards[i].populationDensity = Cards[i].population/Cards[i].area; //Calculating population density.
+        Cards[i].pibPerCapta = Cards[i].PIB/Cards[i].population; //Calculating PIB per capta (sei lá como diabos escreve isso em inglês)
+    };
+//Outputing the values of each card.
+    for (i=0; i<2; i++){
+        printf("\n\nCarta %s\n", Cards[i].cardCode);
+        printf("Populacao de %d habitantes.\n", Cards[i].population);
+        printf("Area de %d Km quadrados.\n", Cards[i].area);
+        printf("PIB de %d Reais.\n", Cards[i].PIB);
+        printf("Atracoes turisticas: %d.\n", Cards[i].touristAttractions);
+        printf("Densidade populacional de %.2f por Km quadrado.\n", Cards[i].populationDensity);
+        printf("PIB per capta de %.2f Reais por habitante.\n\n\n\n", Cards[i].pibPerCapta);
+
+
+    }
 }
