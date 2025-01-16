ffe08476c6b0478df8080a5394bda0b8b46974a0
nik8830 <168482527+nik8830@users.noreply.github.com>
2025-01-16 15:51:03 -0300

    Novo sistema com menu Inicial.
    Implementei um menu inicial que permite criar novas cartas individualmente e também selecionar uma carta já criada para inspeção.
    Ainda faltam:
    
    Comparação de cartas
    Super Poder

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,52 +1,79 @@
 #include <stdio.h>
 
-int main(){
 //Data for each card.
-    struct CartasSuperTrunfo
-    {
-        char cardCode[3];
-        int population;
-        int area;
-        int PIB;
-        int touristAttractions;
-        float populationDensity;
-        float pibPerCapta;
-    };
-    struct CartasSuperTrunfo Cards[10];
+struct CartasSuperTrunfo
+{
+    char cardCode[3];
+    int population;
+    int area;
+    int PIB;
+    int touristAttractions;
+    float populationDensity;
+    float PIBPerCapta;
+};
+struct CartasSuperTrunfo Cards[9];
+int maxCards = 0; //Stores the ammount of cards created.
 
-//Determining how much cards will be registered.
-    int i;
-    int maxCards;
-    printf("Quantas cartas deseja registrar? (Maximo de 10 Cartas)\n");
-    scanf("%d", &maxCards);
+//Comparison Menu.
+void compareCards(int card1, int card2){
 
-//Data input to register each card.   
-    for (i=0; i<maxCards; i++){
-        int numeroCarta = i+1;
-        printf("\nDigite o codigo da carta %d: (Exemplo:A03)\n", numeroCarta);
-        scanf("%s", &Cards[i].cardCode);
-        printf("\nDigite a populacao:\n");
-        scanf("%d", &Cards[i].population);
-        printf("\nDigite a área da cidade em Km quadrados:\n");
-        scanf("%d", &Cards[i].area);
-        printf("\nDigite o PIB da cidade:\n");
-        scanf("%d", &Cards[i].PIB);
-        printf("\nDigite o numero de atracoes turisticas da Cidade:\n");
-        scanf("%d", &Cards[i].touristAttractions);
-        
-        Cards[i].populationDensity = Cards[i].population/Cards[i].area; //Calculating population density.
-        Cards[i].pibPerCapta = Cards[i].PIB/Cards[i].population; //Calculating PIB per capta (sei lá como diabos escreve isso em inglês)
-    };
-//Outputing the values of each card.
-    for (i=0; i<maxCards; i++){
-        printf("\n\nCarta %s\n", Cards[i].cardCode);
-        printf("Populacao de %d habitantes.\n", Cards[i].population);
-        printf("Area de %d Km quadrados.\n", Cards[i].area);
-        printf("PIB de %d Reais.\n", Cards[i].PIB);
-        printf("Atracoes turisticas: %d.\n", Cards[i].touristAttractions);
-        printf("Densidade populacional de %.2f por Km quadrado.\n", Cards[i].populationDensity);
-        printf("PIB per capta de %.2f Reais por habitante.\n\n\n\n", Cards[i].pibPerCapta);
+}
+//Card inspection.
+void cardInspection(int cartaEscolhida){
+    printf("\n\nCarta %s\n", Cards[cartaEscolhida-1].cardCode);
+    printf("Populacao de %d habitantes.\n", Cards[cartaEscolhida-1].population);
+    printf("Area de %d Km quadrados.\n", Cards[cartaEscolhida-1].area);
+    printf("PIB de %d Reais.\n", Cards[cartaEscolhida-1].PIB);
+    printf("Atracoes turisticas: %d.\n", Cards[cartaEscolhida-1].touristAttractions);
+    printf("Densidade populacional de %.2f por Km quadrado.\n", Cards[cartaEscolhida-1].populationDensity);
+    printf("PIB per capta de %.2f Reais por habitante.\n\n\n\n", Cards[cartaEscolhida-1].PIBPerCapta);
+}
 
+//Card selection menu.
+void cardsMenu(){
+    int carta;
+    puts("Escolha uma carta:");
+    for (int i = 0; i<maxCards; i++){
+        printf("%d. %s\n", i+1, Cards[i].cardCode);
+    }
+    scanf("%d", &carta);
+    cardInspection(carta);
+}
+//Card creation menu.
+void cardCreationMenu(){
+    int i=0;
+    printf("\nDigite o codigo da carta: (Exemplo:A03)\n");
+    scanf("%s", &Cards[i].cardCode);
+    printf("\nDigite a populacao:\n");
+    scanf("%d", &Cards[i].population);
+    printf("\nDigite a área da cidade em Km quadrados:\n");
+    scanf("%d", &Cards[i].area);
+    printf("\nDigite o PIB da cidade:\n");
+    scanf("%d", &Cards[i].PIB);
+    printf("\nDigite o numero de atracoes turisticas da Cidade:\n");
+    scanf("%d", &Cards[i].touristAttractions);     
+    Cards[i].populationDensity = Cards[i].population/Cards[i].area; //Calculating population density.
+    Cards[i].PIBPerCapta = Cards[i].PIB/Cards[i].population; //Calculating PIB per capta (sei lá como diabos escreve isso em inglês)
+    i++;
+    maxCards++;
+}
 
+//Initial interaction menu.
+void initialMenu() {
+    int opcao;
+    puts("\nJogo SuperTrunfo\nDigite o numero de uma opcao para continuar.");
+    puts("1. Registrar nova carta.\n2. Visualizar carta.\n3. Comparar duas cartas.");
+    scanf(" %d", &opcao);
+    switch (opcao){
+        case 1:
+        cardCreationMenu();
+        case 2:
+        cardsMenu();
+        case 3:
+        compareCards(1, 2);
     }
 }
+
+int main(){
+    while(1>0){initialMenu();};
+};
